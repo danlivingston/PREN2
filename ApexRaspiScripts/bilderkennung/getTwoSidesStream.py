ad0ba729a00265cd442f8ae7d7bc286fce6f19ab
jzenhaus <jonas.zenhaeusern@stud.hslu.ch>
2024-05-02 10:10:48 +0200

    add cube detection

---
+++ ApexRaspiScripts/bilderkennung/getTwoSidesStream.py
@@ -0,0 +1,53 @@
+import cv2
+from ultralytics import YOLO
+import os
+
+def open_camera_profile(ip_address, username, password, profile):
+    model = YOLO('models/reference_segmentation_2.pt')
+
+    cap = cv2.VideoCapture(
+        f"rtsp://{username}:{password}@{ip_address}/axis-media/media.amp?streamprofile={profile}"
+    )
+
+    if not cap.isOpened():
+        print("Fehler: Der Video-Stream konnte nicht geöffnet werden.")
+        return
+
+    frame_counter = 0
+    saved_front = False
+    saved_back = False
+
+    while True:
+        ret, frame = cap.read()
+        frame_counter += 1
+
+        if not ret:
+            print("Fehler: Es konnte kein Frame gelesen werden.")
+            break
+
+        if frame_counter % 11 == 0:
+            results = model(frame)
+            for r in results:
+                if r.boxes.xyxyn.shape[0] > 0:
+                    print(r.boxes.xyxyn)
+                    if not saved_front and r.boxes.xyxyn[0][0] > 0.48 and r.boxes.xyxyn[0][1] > 0.362:
+                        cv2.imwrite('front_frame.jpg', frame)
+                        saved_front = True
+
+                    if not saved_back and r.boxes.xyxyn[0][0] < 0.27 and r.boxes.xyxyn[0][3] < 0.38:
+                        cv2.imwrite('back_frame.jpg', frame)
+                        saved_back = True
+
+                    if saved_front and saved_back:
+                        print("Beide Bilder wurden gespeichert. Beenden der Schleife.")
+                        cap.release()
+                        cv2.destroyAllWindows()
+                        return
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+open_camera_profile("147.88.48.131", "pren", "463997", "pren_profile_med")
