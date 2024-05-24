71eac2cf1f9dde83aa03ae1db7874f9189be39b7
Dan <dan@loopsites.com>
2024-05-24 10:42:25 +0200

    Update getTwoSidesStream.py

--- ApexRaspiScripts/bilderkennung/getTwoSidesStream.py
+++ ApexRaspiScripts/bilderkennung/getTwoSidesStream.py
@@ -1,14 +1,19 @@
 import cv2
 from ultralytics import YOLO
-from quarter import Quarter
-from circle import Circle
 import os
 
 
 class CubeFaceDetector:
-    def __init__(self, model_path='ApexRaspiScripts/bilderkennung/models/reference_segmentation_2.pt', ip_address="147.88.48.131", username="pren", password="463997", profile="pren_profile_med"):
+    def __init__(
+        self,
+        model_path="ApexRaspiScripts/bilderkennung/models/reference_segmentation_2.pt",
+        ip_address="147.88.48.131",
+        username="pren",
+        password="463997",
+        profile="pren_profile_med",
+    ):
         self.model = YOLO(model_path)
-        self.imgWarmup = 'warmup.jpg'
+        self.imgWarmup = "warmup.jpg"
         self.ip_address = ip_address
         self.username = username
         self.password = password
@@ -16,8 +21,9 @@ class CubeFaceDetector:
 
     def warmupModels(self):
         warmupresultCube = self.model(self.imgWarmup)
+
     def delete_existing_files(self):
-        files_to_delete = ['back_frame.jpg', 'front_frame.jpg']
+        files_to_delete = ["back_frame.jpg", "front_frame.jpg"]
         for file in files_to_delete:
             if os.path.exists(file):
                 os.remove(file)
@@ -50,12 +56,20 @@ class CubeFaceDetector:
                 for r in results:
                     if r.boxes.xyxyn.shape[0] > 0:
                         print(r.boxes.xyxyn)
-                        if not saved_front and r.boxes.xyxyn[0][0] > 0.35 and r.boxes.xyxyn[0][1] > 0.35:
-                            cv2.imwrite('front_frame.jpg', frame)
+                        if (
+                            not saved_front
+                            and r.boxes.xyxyn[0][0] > 0.35
+                            and r.boxes.xyxyn[0][1] > 0.35
+                        ):
+                            cv2.imwrite("front_frame.jpg", frame)
                             saved_front = True
 
-                        if not saved_back and r.boxes.xyxyn[0][2] < 0.65 and r.boxes.xyxyn[0][3] < 0.41:
-                            cv2.imwrite('back_frame.jpg', frame)
+                        if (
+                            not saved_back
+                            and r.boxes.xyxyn[0][2] < 0.65
+                            and r.boxes.xyxyn[0][3] < 0.41
+                        ):
+                            cv2.imwrite("back_frame.jpg", frame)
                             saved_back = True
 
                         if saved_front and saved_back:
@@ -63,7 +77,7 @@ class CubeFaceDetector:
                             cv2.destroyAllWindows()
                             return
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
         cap.release()
@@ -73,6 +87,6 @@ class CubeFaceDetector:
         self.open_camera_profile()
 
 
-#detector = CubeFaceDetector()
-#detector.warmupModels()
-#detector.start_detection()
+# detector = CubeFaceDetector()
+# detector.warmupModels()
+# detector.start_detection()
